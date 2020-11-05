c71f2193ae91262d39355f21d02e02de3395933a
Alvin <anguyen8613@gmail.com>
2020-11-05 15:21:06 -0800

    Initiate server, client, redux, mongodb

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.lkf3r.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((e) => console.log(e.message));
+
+mongoose.set('useFindAndModify', false);
