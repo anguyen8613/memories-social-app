2195a1921e7159fca9dcd74589b2c30b5dd7d183
Alvin <anguyen8613@gmail.com>
2020-11-05 15:32:29 -0800

    Add client

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import makeStyles from './styles';
+
+const Post = () => {
+    const classes = makeStyles();
+    return(
+        <h1>POST</h1>
+    )
+}
+
+export default Post;
