60443da40018d864b726b488e68e7617ddc5113f
Alvin <anguyen8613@gmail.com>
2020-11-05 18:02:48 -0800

    Add post message api

---
+++ client/src/actions/posts.js
@@ -0,0 +1,20 @@
+import * as api from '../api';
+
+export const getPosts = () => async(dispatch) => {
+    try{
+        const {data} = await api.fetchPosts();
+        dispatch({type: 'FETCH_ALL', payload: data});
+    }catch(e){
+        console.log(e.message);
+    }
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try{
+        const{data} = await api.createPost(post);
+        dispatch({type: 'CREATE', payload: data});
+        console.log(data);
+    }catch(e){
+        console.log(e.message);
+    }
+}
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -11,11 +11,11 @@ export const getPosts = async(req, res) => {
 }
 
 export const createPost = async(req, res) => {
-    const post = req.body;
-    const newPost = new PostMessage(post);
+    const { title, message, selectedFile, creator, tags } = req.body;
+    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
     try{
-       await newPost.save();
-       res.status(201).json(newPost);
+       await newPostMessage.save();
+       res.status(201).json(newPostMessage);
     }catch(e){
         res.status(409).json({message: e.message})
     }
